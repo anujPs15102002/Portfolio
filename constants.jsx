4711f18b26c4133e9d30301ecf2c5f6202894217
Anuj Pratap Singh <161419572+anujPs15102002@users.noreply.github.com>
2025-03-06 12:27:51 +0530

    Add files via upload

---
+++ constants.jsx
@@ -0,0 +1,292 @@
+export const Bio = {
+  name: "Anuj Pratap Singh",
+  roles: ["front end web developer", "Programmer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/anujPs15102002",
+  resume:
+    "file:///C:/Users/Narendra%20singh/Downloads/Anuj_Resume.pdf",
+  linkedin:
+    "https://www.linkedin.com/in/anuj-pratap-singh-764913250?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  twitter: "https://x.com/AnujPra30934246?t=3Z0zaMdyoW9pAA9oQBbt5g&s=08",
+  insta: "https://www.instagram.com/anujpratapsi?igsh=MWYybms5aDUycnIxdQ==",
+  facebook: "https://www.facebook.com/share/sD9VFpTQr1mGjfrw/?mibextid=qi2Omg",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "https://tse1.mm.bing.net/th?id=OIP.K-4RqDC6zFrpAG31ayDDOgHaHa&pid=Api&P=0&h=180",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Talwind",
+        image:
+          "https://img.icons8.com/?size=100&id=4PiNHtUJVbLs&format=png&color=000000",
+      },
+    ],
+  },
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://imgs.search.brave.com/Q--MjQmzhJDTZDFvE-IIVOPn43ZvWusW1XgMpBKbYZ8/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9icmFu/ZHNsb2dvcy5jb20v/d3AtY29udGVudC91/cGxvYWRzL2ltYWdl/cy9qYXZhLWxvZ28t/MS5wbmc ",
+      },
+      {
+        name: "Data Structures",
+        image:
+          "https://cdn2.iconfinder.com/data/icons/data-organisation-flat/60/Structured-Data-structured-database-databases-hierarchy-1024.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://imgs.search.brave.com/Y1n4MpRFktzIe412ziEQ-3lsL8F1jQv-FGbFQrS8Ob8/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuc3RpY2twbmcu/Y29tL2ltYWdlcy81/ODQ4MTUyZmNlZjEw/MTRjMGI1ZTQ5Njcu/cG5n",
+      },
+      {
+        name: "OOPS",
+        image:
+          "https://miro.medium.com/v2/resize:fit:577/0*s9L1Z_u9WEt9corO.png",
+      },
+      {
+        name: "SQL",
+        image: "https://cdn-icons-png.flaticon.com/128/2772/2772128.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "pg Admin 4",
+        image:
+          "https://tse1.mm.bing.net/th?id=OIP.n4t15L8-493hIMdHtIPG0QAAAA&pid=Api&P=0&h=180",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+    role: "Frontend Engineer Intern",
+    company: "Neurobit",
+    date: "June 2023 - Nov 2023",
+    desc: "Enhanced user experiences on Neurobit PSG & Hybrid, Portals by resolving bugs & reduced load time by 40%. Built Neurobit Analytics portal using React Js with seamless interaction of REST APIs using AXIOS optimized with React Query. Refactored previous code to TypeScript, updated with new dependency and integrated Vite with Jest for Unit Testing.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Screenshot%20from%202024-01-25%2022-38-31.png?alt=media&token=2785903f-1a4e-41f5-afd2-6adcfe56d058",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "DevOps & Fullstack Engineering Intern",
+    company: "Flipr Inovations Pvt. Ltd.",
+    date: "Aug 2023 - Oct 2023",
+    desc: "Built Flipr Connect Platforms using React Js integrated GraphQL with AXIOS, created High level Design and Figma design. Built Backend with GraphQL and Node JS and connected with MongoDb - Reducing API calls by 20%. Dockerized and automated with Kubernetes & Jenkins CI/CD deployed in AWS-EC2 added Prometheus & Grafana for monitoring.",
+    skills: [
+      "Docker",
+      "Terraform",
+      "AWS",
+      "EC2",
+      "Portainer",
+      "Nginx",
+      "JavaScript",
+      "TypeScript",
+      "Node Js",
+      " Next Js",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/1696514649120.jpeg?alt=media&token=e7f6757b-edfa-4138-a692-d6709eeef3e2",
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
+    role: "Android Developer Intern",
+    company: "Rudraksha Welffare Foundation",
+    date: "June 2021 - Oct 2021",
+    desc: "• Built RudraShakti - Astrologer App, with MVVM Architecture using Java and Android Studio integrating Firebase SDK. Created One to One video call integration with Socket.IO and Firebase cloud functions and integrated with Retrofit. Created Low Level Design and converted Figma design to XML code.",
+    skills: [
+      "Android",
+      "Java",
+      "Kotlin",
+      "XML",
+      "Node Js",
+      "Cloud Firestore",
+      "Firebase",
+      "Figma",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+  },
+  {
+    id: 3,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Fullstack Externship",
+    company: "Flipr",
+    date: "June 2023 - July 2023",
+    desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/1691181448873.jpeg?alt=media&token=ee85eb8f-7247-43cd-9a1d-ce9f58ea62a6",
+  },
+  {
+    id: 4,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+    role: "Android Developer",
+    company: "DSC KIIT",
+    date: "Nov2021 - Present",
+    desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+    skills: [
+      "Leadership",
+      "Mobile Application Development",
+      "Kotlin",
+      "XML",
+      "Figma",
+    ],
+  },
+  {
+    id: 5,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "GirlScript Summer of Code",
+    date: "May 2023 - Present",
+    desc: "Contributed to different open-source projects and learn from industry experts",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://imgs.search.brave.com/3Lap7Eu2vUJd_E8V6f7gJYgUem1LgkR-UzwEd17RV6w/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Z2xiYWphamdyb3Vw/Lm9yZy9pbWcvYWJv/dXQtdXMvbmV3LWxv/Z28tZ2xiYWphai53/ZWJw",
+    school: "G.L. Bajaj Group Of Institutions",
+    date: "2021 - 2025",
+    grade: "77.9%",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at G.L. Bajaj Group Of Institurions, Mathura. I am currently in my 4th year and performing well in my studies. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://imgs.search.brave.com/YaojFZ-4a2jtcmYKMogLcWLKc3FWSqvZVb2AFqHOI3o/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9wbGF5/LWxoLmdvb2dsZXVz/ZXJjb250ZW50LmNv/bS9HSDNaYkhrTHkt/djA5X0pLYlNLZ2Fy/cEk0LUJ4cUx6U0My/NFk5WTBjTnMzV3JE/M0E0RzU0dksyTDFq/OFo1eW0xTi1j.jpeg",
+    school: "Vidya Devi Jindal School, Kosi Kalan, Mathura",
+    date: "Apr 2021",
+    grade: "75%",
+    desc: "I completed my class 12 high school education at Vidya Devi Jindal School, Mathura, where I studied Science with Computer Science.",
+    degree: "XII, Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://imgs.search.brave.com/YaojFZ-4a2jtcmYKMogLcWLKc3FWSqvZVb2AFqHOI3o/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9wbGF5/LWxoLmdvb2dsZXVz/ZXJjb250ZW50LmNv/bS9HSDNaYkhrTHkt/djA5X0pLYlNLZ2Fy/cEk0LUJ4cUx6U0My/NFk5WTBjTnMzV3JE/M0E0RzU0dksyTDFq/OFo1eW0xTi1j.jpeg",
+    school: "Vidya Devi Jindal School, Kosi Kalan, Mathura",
+    date: "Apr 2019",
+    grade: "85%",
+    desc: "I completed my class 10 education at Vidya Devi Jindal School, Mathura, where I studied Science with Computer Application.",
+    degree: "X, Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Task Tracker Web App",
+    date: "10-2024",
+    description:"# Task Tracker Web App Using React,enabling user to effectiveley create,edit and delete task.Implemented a responsive user Interface for optimal user experience across devices.Managed state effectively using hooks and context API. Integrated local storage for persistent data management ,preversing task after page refresh . Utilized Component based Archtecture for reusable User Interface Componenets,enhencing maintainability and scalability  ",
+                
+      
+    image: 
+    "https://res.cloudinary.com/monday-blogs/w_1193,h_675,c_fit/fl_lossy,f_auto,q_auto/wp-blog/2020/12/1-10.png",
+    tags: ["React Js","Local Storage"],
+    category: "web app",
+    github: "https://github.com/anujPs15102002/Task-Tracker-Web-App",
+    /*webapp: "https://decisionhub.netlify.app/",*/
+  },
+  {
+    id: 9,
+    title: "Flix Analyzer",
+    date: "01-2025",
+    description:
+      "# Utilized MySQL for efficient storage and retrieval of large film datasets, including genres, user ratings, and viewing histories. Implemented analytical queries and data visualization techniques to generate insights on audience engagement and content performance.Enhanced user experience with tailored recommendations based on data trends. Designed for scalability to support growing datasets in streaming services. Demonstrated SQL application for data analysis at the intersection of technology and media consumption.",
+    image:
+      "https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/building-your-data-analyst-portfolio.png",
+    tags: ["MYSQL"],
+    category: "Data Analysis",
+    github: "https://github.com/anujPs15102002/netflix_sql_project",
+   /* webapp: "https://trackify-management.netlify.app/",*/
+  },
+ /* {
+    id: 0,
+    title: "Hospital Management System",
+    date: "",
+    description:
+      "Develop a comprehensive Hospital Management System usgin java to streaming hospital operations and enhance pateint care it improves overall efficiency of hospital operations by providing a central system for managing all aspects of hospital management showcasing strong skills in Java. Data Structures, Database(MYSQL) for storing and managing data Frameworks(JavaFX) for user interface JDBC for database connectively",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+    tags: ["React Js", "HTML", "CSS", "Javascript"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Podstream",
+    webapp: "https://podstream.netlify.app/",
+    member: [],
+  },*/
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
