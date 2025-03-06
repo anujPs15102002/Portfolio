c6c3ec1195de86cf248cc7b078bffba02237187c
Anuj Pratap Singh <161419572+anujPs15102002@users.noreply.github.com>
2025-03-06 11:49:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,60 @@
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/sections/Hero";
+import Skills from "./components/sections/Skills";
+import Experience from "./components/sections/Experience";
+import Education from "./components/sections/Education";
+import StartCanvas from "./components/canvas/Stars";
+import Projects from "./components/sections/Projects";
+import Contact from "./components/sections/Contact";
+import Footer from "./components/sections/Footer";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+  position: relative;
+`;
+
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+    to top right,
+    rgba(16, 55, 131, 0.2) 10%,
+    rgba(16, 55, 131, 0.1) 30%,
+    rgba(0, 0, 0, 0.8) 80%,
+    rgba(0, 0, 0, 1) 100%
+  );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar />
+        <Body>
+          <StartCanvas />
+          <div>
+            <Hero />
+            <Wrapper>
+              <Skills />
+              {/* <Experience /> */}
+            </Wrapper>
+            <Projects />
+            <Wrapper>
+              <Education />
+              <Contact />
+            </Wrapper>
+            <Footer />
+          </div>
+        </Body>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
